dac72e583bb54a18ff0420225c022aa6f9126948
konstantin.dubanevich <k.dubanevich@belitsoft.com>
2025-09-18 23:33:33 +0300

    Initial commit

---
+++ src/Task1/Main.java
@@ -0,0 +1,34 @@
+package Task1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первую строку:");
+        String string1 = sc.nextLine();
+        System.out.println("Введите вторую строку:");
+        String string2 = sc.nextLine();
+        System.out.println("Введите третью строку:");
+        String string3 = sc.nextLine();
+
+        String[] stringArray = new String[] {string1, string2, string3};
+        String maxString = "";
+        String minString = stringArray[0];
+
+        for (int i = 1; i < stringArray.length; i++) {
+            if (stringArray[i].length() > maxString.length()) {
+                maxString = stringArray[i];
+            }
+            if (stringArray[i].length() < minString.length()) {
+                minString = stringArray[i];
+            }
+        }
+
+        System.out.println("Самая длинная строка размером " + maxString.length() + ":");
+        System.out.println(maxString);
+        System.out.println("Самая короткая строка размером " + minString.length() + ":");
+        System.out.println(minString);
+    }
+}
